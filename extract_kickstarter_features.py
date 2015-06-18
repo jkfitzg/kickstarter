4a36e0232c899fd63045c40bae2289ca81b4cc73
Jamie Fitzgerald <jamie.fitzgerald@gmail.com>
2015-06-17 20:16:45 -0400

    more features, working decision trees

--- extract_kickstarter_features.py
+++ extract_kickstarter_features.py
@@ -2,6 +2,11 @@ import re
 import json
 
 
+import time
+import datetime
+import numpy as np
+from scipy import stats
+
 def get_creator_activity(creator_text_only):
     # initialize for -1 so I can see where my parsing failed
     n_backed = -1
@@ -31,6 +36,7 @@ def analyze_rewards(project_text):
     # now get reward summaries
     n_rewards = len(reward_int_list)
     min_reward = min(reward_int_list)
+    median_reward = median(reward_int_list)
     max_reward = max(reward_int_list)
     rewards_json = json.dumps(reward_int_list) 
 
@@ -53,4 +59,60 @@ def analyze_rewards(project_text):
         delivery_list = ['-1']
     shipping_json = json.dumps(delivery_list)
 
-    return [n_rewards, min_reward, max_reward, rewards_json, shipping_json, intn_ship]
+    return [n_rewards, min_reward,median_reward, max_reward, rewards_json, shipping_json, intn_ship]
+
+def get_time_features(unix_t_start):
+    # weekdays are index 0=Monday
+    # just truncate hour (0-23) for now; ignore minutes
+
+    t_struct = time.localtime(unix_t_start)  
+
+    # month, day of month, day of week, hour
+    return t_struct.tm_mon, t_struct.tm_mday, t_struct.tm_wday, t_struct.tm_hour 
+
+def calc_days_to_reward(rewards_ship_dates_str,campaign_start_unix_t):
+    # I think months to reward is more useful, but low roi. leave as is. 
+    # convert to list
+    rewards_ship_dates = json.loads(rewards_ship_dates_str)
+
+    if rewards_ship_dates[0] == '-1':
+        return -1
+
+    ship_ts_unix_t = np.empty_like(rewards_ship_dates,dtype=int)
+
+    month_str_to_int = {'Jan':1,\
+                        'Feb':2,\
+                        'Mar':3,\
+                        'Apr':4,\
+                        'May':5,\
+                        'Jun':6,\
+                        'Jul':7,\
+                        'Aug':8,\
+                        'Sep':9,\
+                        'Oct':10,\
+                        'Nov':11,\
+                        'Dec':12}
+
+    # convert all rewards_ship_dates
+    for i, ship_d_str in enumerate(rewards_ship_dates):
+        month_str, year_str = ship_d_str.split()
+
+        month_int = month_str_to_int[month_str]
+        year_int = int(year_str)
+
+        dt = datetime.datetime(year_int, month_int, 1) #assume day 1
+        ship_ts_unix_t[i] = time.mktime(dt.utctimetuple())
+
+    # get mode ship time
+    min_ship_unix_t = stats.mode(ship_ts_unix_t)
+
+    s_to_ship = min_ship_unix_t - campaign_start_unix_t
+    # if negative, convert to 0. 
+    if s_to_ship < 0:
+        s_to_ship = 0
+
+    s_per_day = 86400
+
+    days_to_ship = round(float(s_to_ship)/s_per_day)
+    return days_to_ship
+
